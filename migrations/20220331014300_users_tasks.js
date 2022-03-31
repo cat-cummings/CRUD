03f50054f1147cd17502766539225db373abeb4b
Cat Cummings <96845535+cat-cummings@users.noreply.github.com>
2022-03-31 14:47:43 +1300

    Added migrations

---
+++ migrations/20220331014300_users_tasks.js
@@ -0,0 +1,11 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('users_tasks', (table) => {
+    table.increments('id').primary
+    table.int('user_id')
+    table.int('task_id')
+  })
+}
+
+exports.down = function (knex) {
+  return knex.schema.dropTable('users_tasks')
+}
